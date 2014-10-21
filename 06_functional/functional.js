1fc4d05436ab62f8f9635c7eda518817942fe00d
faisal <faisalnwz01@gmail.com>
2014-10-21 13:02:21 -0400

    2nd week submission

--- 06_functional/functional.js
+++ 06_functional/functional.js
@@ -37,9 +37,76 @@ var contains = function(array, identifier) {
 	for (var i=0; i < array.length; i++) {
 		if (array[i] == identifier) {
 			return true;
-		}
-		else {
-			return false;
-		};
+		}	
 	}
+		return false;
 };
+
+function countWordsInReduce(array, start) {
+	return countWords(array) + start;
+}
+
+function reduce(array, start ,reducer) {
+var total = start
+forEach(array, function(element) {
+	total = reducer(element, total);
+})
+return total;
+};
+
+function reduce1(array, combine, start) {
+  var current = start;
+  for (var i = 0; i < array.length; i++)
+    current = combine(current, array[i]);
+  return current;
+}
+
+function sum(integers) {
+	return reduce(integers, 0, function(a, b) {
+    return a + b;
+	})
+};
+
+function every(array, func) {
+	var value = true;
+	for (var i = 0; i < array.length; i++) {
+		
+			if (func(array[i]) == false) {
+				value = false;
+			}	
+	}
+	return value
+};
+
+
+
+
+
+function once(inputfunc) {
+	
+
+	var number = 0
+	return function() {
+		inputfunc
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
